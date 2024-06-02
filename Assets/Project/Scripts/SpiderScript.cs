cea01bc95f2f75b626124164536aa511a622d60a
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2024-06-02 22:31:48 +0800

    updated spider movement and grabbing function
    updated the spider movement to run away from the player if at a certain distance
    created the grabbing function to grab, move the spider around and throw it

--- Assets/Project/Scripts/SpiderScript.cs
+++ Assets/Project/Scripts/SpiderScript.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,87 +6,149 @@ using UnityEngine.InputSystem;
 
 public class SpiderScript : MonoBehaviour
 {
-
+    // Setting up stuff, agent to tell the AI what to do, animator to bind the animator to the spider
     private NavMeshAgent agent;
     private Animator animator;
+
+    //player to indicate the player
     [SerializeField] private GameObject player;
+    //scare objects for logic for spider to run away when you get close to it
+    [SerializeField] private GameObject escapeObj;
+    [SerializeField] private float scareDistance = 1.5f;
+    //grabbing stuff for grabbing
+    [SerializeField] private bool isGrabbed = false, isPlayerNear = false;
 
-    private bool StepCounter = false;
-
-
-    [SerializeField] /*serializeField makes it show up on unity under the inspector so we can edit it directly there*/ private float minCoord = -4.5f;
+    //coordinates and time for spider travelling
+    [SerializeField]  private float minCoord = -4.5f; /*serializeField makes it show up on unity under the inspector so we can edit it directly there*/
     [SerializeField] private float maxCoord = 4.5f;
+    [SerializeField] private float minWaitTime = 3f;
+    [SerializeField] private float maxWaitTime = 7f;
+
+    //a runnable (cs2030s wow), to change if spider is running away from you or just roaming
+    private Coroutine walkTask = null, escapeTask;
 
     void Start()
     {
+        //initialising to assign the components from unity inspector
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        StartCoroutine(GoRandomPlace(minCoord, maxCoord, 1));
+
+        //starting the walking inside the coordinates at an animation walking speed of 1
+        walkTask = StartCoroutine(GoRandomPlace(minCoord, maxCoord, 1));
     }
 
-    void FixedUpdate()
-    {
-        StartCoroutine(MovementNearPlayer());
-    }
+    /// <summary>
+    /// makes the spider roam around,
+    /// takes in upper and lower coordinates for you to limit the range of spider and speed of animation settings(0,1,2)
+    /// returns the runnable to execute it
+    /// </summary>
     public IEnumerator GoRandomPlace(float lower, float upper, int speed)
     {
+        Debug.Log("Walking normally === ");
         //generate a random max and min coordinate
         float x = UnityEngine.Random.Range(lower, upper), z = UnityEngine.Random.Range(lower, upper);
 
-        Debug.Log("Now pathed to " + x + " and " + z);
+        //Debug.Log("Now pathed to " + x + " and " + z);
 
-        //set the spider to travel to a random location
-        agent.SetDestination(new Vector3(x, transform.position.y, z));
+        //generate coordinates for a new random position
+        Vector3 newRandomPos = new Vector3(x, transform.position.y, z);
+        //set the destination of travel to the new position
+        agent.SetDestination(newRandomPos);
+        //teleporting the marker to where the spider is moving towards(just to see where the spider is going for debugging)
+        escapeObj.transform.position = newRandomPos;
 
-        //set the movespeed
+        //set the animation move speed
         animator.SetInteger("MoveSpeed", speed);
 
+        //waiting for calculations of the new coordinates so it starts walking
         yield return new WaitForSeconds(2f);
 
         // Has spider reached? If not, keep pausing the code here
         while (agent.remainingDistance > 0.1f)
         {
-            Debug.Log("WAITING");
+            //Debug.Log("WAITING");
             yield return new WaitForFixedUpdate();
         }
 
-        Debug.Log("Stopped " + agent.remainingDistance);
-
+        //Debug.Log("Stopped " + agent.remainingDistance);
 
         // Spider Reached! Set to Idle and wait random seconds then do another random movement
         animator.SetInteger("MoveSpeed", 0);
 
-        float randomWaitTime = UnityEngine.Random.Range(3f, 7f);
-        Debug.Log("Reached! Now waiting for " + randomWaitTime + " before selecting another destination");
+        float randomWaitTime = UnityEngine.Random.Range(minWaitTime, maxWaitTime);
+        //Debug.Log("Reached! Now waiting for " + randomWaitTime + " before selecting another destination");
         yield return new WaitForSeconds(randomWaitTime);
 
-        StepCounter = false;
-        StartCoroutine(GoRandomPlace(lower, upper, speed));
+        //recurse
+        walkTask = StartCoroutine(GoRandomPlace(lower, upper, speed));
     }
 
-    public IEnumerator MovementNearPlayer()
+    void FixedUpdate()
     {
-        if(StepCounter == true) {
-            yield return new WaitForFixedUpdate();
+        // Constantly check 50 times a second to see if player is nearby. if it is, trigger an escape task.
+        if (!isGrabbed && Vector3.Distance(player.transform.position, transform.position) < scareDistance) {
+            // Debug.Log("Player is near");
+            if (escapeTask == null) escapeTask = StartCoroutine(RunFromPlayerTask());
         }
-
-        //get location of the spider and player
-        Vector3 spiderPos = transform.position;
-        Vector3 playerPos = player.transform.position;
-        //gets the distance between them
-        float distanceBetwSpiderNPlayer = Vector3.Distance(spiderPos, playerPos);
-        Debug.Log("Distance: " + distanceBetwSpiderNPlayer);
-        //keep recalculating the next position if near the player and move to that point
-        if (distanceBetwSpiderNPlayer < 0.5)
-        {
-            StartCoroutine(GoRandomPlace(minCoord, maxCoord, 2));
-            StepCounter = true;
-            Debug.Log("StepCounter: "  + StepCounter);
-            yield return new WaitForFixedUpdate();
-            
-        }
-        StepCounter = false;
     }
+
+    //javadoc tbc cus im lazy lmao
+    public IEnumerator RunFromPlayerTask()
+    {
+        // Debug.Log("Running!");
+        //stop the current task of the spider aka walking
+        StopCoroutine(walkTask);
+        // Logic for escaping, anywhere between 5 to 10 meters
+        float escapeRange = Random.Range(5, 10);
+        // finds a random point inside the sphere
+        Vector3 randomPointInSphere = Random.insideUnitSphere * escapeRange;
+
+        //translate the random point to navmeshing(finds the closest point in navmesh that fits the rando point in sphere)
+        randomPointInSphere += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomPointInSphere, out hit, escapeRange, 1);
+        Vector3 finalPosition = hit.position;
+        escapeObj.transform.position = finalPosition;
+        //set the destination
+        agent.SetDestination(finalPosition);
+
+        // See if spider has reached the escape destination
+        yield return new WaitForSeconds(2f);
+
+        // Has spider reached? If not, keep pausing the code here
+        while (agent.remainingDistance > 0.1f) yield return new WaitForFixedUpdate();
+
+        // Spider reached! Is player still near it? (i.e. still chasing it the whole while)
+        if (isPlayerNear)
+        {
+            escapeTask = StartCoroutine(RunFromPlayerTask());
+            // Debug.Log("Escape failed, trying to run again...");
+        }
+        // Player no longer chasing it! Resume normal walking tasks
+        else
+        {
+            // Debug.Log("Escape success!");
+            escapeTask = null;
+            walkTask = StartCoroutine(GoRandomPlace(minCoord, maxCoord, 1));
+        }
+    }
+
+    //javac doc tbc but just stopping the task
+    public void StopPathing()
+    {
+        StopCoroutine(walkTask);
+        agent.isStopped = true;
+        isGrabbed = true;
+    }
+
+    //javac doc tbc but just restarting the task
+    public void StartPathingAgain()
+    {
+        isGrabbed = false;
+        walkTask = StartCoroutine(GoRandomPlace(minCoord, maxCoord, 1));
+        
+    }
+
 }
 
 
