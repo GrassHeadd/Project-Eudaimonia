328b0c9de3bba5327fbbf8907cc756095a8ec96c
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2024-07-17 18:31:12 +0800

    asset update for stone and level system update

--- Assets/Project/Scripts/LevelHandleScript.cs
+++ Assets/Project/Scripts/LevelHandleScript.cs
@@ -21,9 +21,14 @@ public class LevelHandleScript : MonoBehaviour
     public void NextLevel() {
         //int sceneIndex = GameObject.FindGameObjectWithTag("StaticGameObject").GetComponent<StaticSceneData>().LastDeathSceneIndex;
         int levelIndex = GameObject.FindGameObjectWithTag("StaticGameObject").GetComponent<StaticSceneData>().LevelIndicator++; 
-        if(levelIndex < 4) {SceneManager.LoadSceneAsync(1);} 
-        if(levelIndex <= 8 && levelIndex >=4) {SceneManager.LoadSceneAsync(2);} 
-        if(levelIndex > 8 && levelIndex < 13) {SceneManager.LoadSceneAsync(3);} 
-        SceneManager.LoadSceneAsync(0);
+        if(levelIndex < 4) {
+            SceneManager.LoadSceneAsync(1);
+            } else if(levelIndex <= 8 && levelIndex >=4) {
+                SceneManager.LoadSceneAsync(2);
+            } else if(levelIndex > 8 && levelIndex < 13) {
+                SceneManager.LoadSceneAsync(3);
+            } else {
+                SceneManager.LoadSceneAsync(0);
+            }
     }
 }
